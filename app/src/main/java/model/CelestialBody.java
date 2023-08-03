aafa71f5a49f3cbcefa387a842039468ad1f37ee
Samuel <samuel.mevik@outlook.com>
2023-08-03 12:29:27 +0200

    Feat: Each celestial child adopts a name

--- app/src/main/java/model/CelestialBody.java
+++ app/src/main/java/model/CelestialBody.java
@@ -4,8 +4,10 @@ package model;
  * A celestial body represents a body residing in space.
  */
 public abstract class CelestialBody {
+  private final String type;
   protected String name;
   protected double radius;
+  
 
   /**
    * Creates a new celestial body.
@@ -13,9 +15,14 @@ public abstract class CelestialBody {
    * @param name  The name of the celestial body.
    * @param radius The radius of the celestial body.
    */
-  public CelestialBody(String name, double radius) {
+  public CelestialBody(String type, String name, double radius) {
+    this.type = type;
     this.name = name;
     this.radius = radius;
+  }
+
+  public String getType() {
+    return type;
   }
 
   public String getName() {
